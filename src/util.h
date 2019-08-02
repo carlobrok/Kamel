47d237746e9450263c05bb8dc7381b3a7b4015ec
carlobrok <carlo.brokering@outlook.de>
2019-08-02 17:57:17 +0200

    Removed old log functions

--- src/util.h
+++ src/util.h
@@ -4,23 +4,18 @@
 #include <thread>
 #include "opencv2/opencv.hpp"
 
-
 inline void log_timing(int64 & tlast, const char* message) {
 	std::cout << message << (cv::getTickCount() - tlast) / cv::getTickFrequency() * 1000.0 << " ms" << std::endl;
 	tlast = cv::getTickCount();
 }
 
-//void init_sensorlog();
-
-void log_sensordata(std::vector<cv::Point> & line_points, int & grstate, cv::Point & grcenter, cv::Mat & img_output);
-
 bool inMat(cv::Point p, int w, int h);
 
-inline void thread_delay(int64 ms) {
+inline void thread_delay(int64_t ms) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-inline void thread_delay_micros(int64 us) {
+inline void thread_delay_micros(int64_t us) {
 	std::this_thread::sleep_for(std::chrono::microseconds(us));
 }
 
