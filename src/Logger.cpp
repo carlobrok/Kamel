d36953b926d083ed0dc6866550140eafad4e28d6
carlobrok <carlo.brokering@outlook.de>
2019-08-14 16:27:33 +0200

    New logger

---
+++ src/Logger.cpp
@@ -0,0 +1,30 @@
+#include "Logger.h"
+#include "boost/filesystem.hpp"
+
+namespace fs = boost::filesystem;
+
+Logger::Logger(std::string channel_name) {
+	fs::path cur_path = fs::current_path().append("logs");
+	if(!fs::exists(cur_path)) {
+		fs::create_directory(cur_path);
+	}
+
+	_logger = spdlog::basic_logger_mt<spdlog::async_factory>(channel_name, "logs/" + channel_name + ".log");
+	_logger->set_pattern("[%^%l%$] %T %v");
+}
+
+
+void Logger::log(std::string msg, spdlog::level::level_enum lvl) {
+	msg = std::to_string(cur_sec()) + " " + msg;
+	_logger->log(lvl, msg);
+	_logger->flush();
+}
+
+void Logger::log_buffer(spdlog::level::level_enum lvl) {
+	if(!_buffer.empty()) {
+		_buffer = std::to_string(cur_sec()) + " " + _buffer;
+		_logger->log(lvl, _buffer);
+		_logger->flush();
+		_buffer.clear();
+	}
+}
---
+++ src/Logger.h
@@ -0,0 +1,68 @@
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include <string>
+
+#include "spdlog/async.h" //support for async logging.
+#include "spdlog/sinks/basic_file_sink.h"
+
+#include "opencv2/opencv.hpp"
+#include "util.h"
+
+class Logger {
+private:
+
+	std::shared_ptr<spdlog::logger> _logger;
+	std::string _buffer = "";
+
+public:
+	Logger(std::string log_name);
+
+	void log(std::string msg, spdlog::level::level_enum lvl);
+	void log_buffer(spdlog::level::level_enum lvl);
+
+	template< typename T >
+	Logger& operator<<(const T& val) {
+		_buffer += val;
+		return *this;
+	}
+
+	Logger& operator<<(const int& i) {
+		_buffer += std::to_string(i);
+		return *this;
+	}
+
+	Logger& operator<<(const long& l) {
+		_buffer += std::to_string(l);
+		return *this;
+	}
+
+	Logger& operator<<(const double& d) {
+		_buffer += std::to_string(d);
+		return *this;
+	}
+
+	Logger& operator<<(const float& f) {
+		_buffer += std::to_string(f);
+		return *this;
+	}
+
+	Logger& operator<<(const cv::Point& p) {
+		_buffer += "("  + std::to_string(p.x) + "," + std::to_string(p.y) + ")";
+		return *this;
+	}
+
+	Logger& operator<<(const spdlog::level::level_enum& lvl) {
+		if(!_buffer.empty()) {
+			_buffer = std::to_string(cur_sec()) + " " + _buffer;
+			_logger->log(lvl, _buffer);
+			_logger->flush();
+			_buffer.clear();
+		}
+		return *this;
+	}
+
+};
+
+
+#endif
