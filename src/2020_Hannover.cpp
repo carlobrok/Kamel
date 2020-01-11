4efc0c1172d985d627b5de2e4c75f85668f93d12
carlobrok <carlo.brokering@outlook.de>
2020-01-11 01:48:39 +0100

    Added histogram of HSV for quick inspection

--- src/2020_Hannover.cpp
+++ src/2020_Hannover.cpp
@@ -272,6 +272,9 @@ void image_processing() {
 	srv.namedWindow("Mask SW");
 	srv.namedWindow("HSV");
 	srv.namedWindow("Input");
+	srv.namedWindow("H-S Histogram");
+
+
 
 	int64 tloop;
 
@@ -284,6 +287,43 @@ void image_processing() {
 		// Filter image and convert to hsv
 		GaussianBlur(img_rgb, img_rgb, Size(5,5),2,2);		// Gaussian blur to normalize image
 		cvtColor(img_rgb, hsv, COLOR_BGR2HSV);			// Convert to HSV and save in Mat hsv
+
+//#ifdef PLOT_HSV_HISTOGRAM
+
+		// Histogramm der HSV Matrix ausgeben (wenn PLOT_HSV_HISTOGRAM definiert ist)
+
+		vector<Mat> bgr_planes;
+		split( hsv, bgr_planes );
+		int histSize = 256;
+		float range[] = { 0, 256 }; //the upper boundary is exclusive
+		const float* histRange = { range };
+		bool uniform = true, accumulate = false;
+		Mat b_hist, g_hist, r_hist;
+		calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+		calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+		calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+		int hist_w = 512, hist_h = 400;
+		int bin_w = cvRound( (double) hist_w/histSize );
+		Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 255,255,255) );
+		normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+		normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+		normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+		for( int i = 1; i < histSize; i++ )
+		{
+				line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ),
+							Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+							Scalar( 255, 0, 0), 2, 8, 0  );
+				line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ),
+							Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+							Scalar( 0, 255, 0), 2, 8, 0  );
+				line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ),
+							Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+							Scalar( 0, 0, 255), 2, 8, 0  );
+		}
+
+
+//#endif
+
 
 		//		log_timing(tlast, "Reading, color covert, gauss: ");
 
@@ -300,6 +340,8 @@ void image_processing() {
 		//		log_timing(tlast, "Green calc: ");
 
 		// Alle Bilder an VideoServer übergeben
+
+		srv.imshow("H-S Histogram", histImage );
 		srv.imshow("Input", img_rgb);
 		srv.imshow("HSV", hsv);
 		srv.imshow("Mask SW", bin_sw);
