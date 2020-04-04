ddd3918dec6ed19d760cb9603ac50368db07602e
carlobrok <carlo.brokering@outlook.de>
2020-04-04 20:46:06 +0200

    i2c library has to be linked

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,4 +4,4 @@
 
 USER_OBJS :=
 
-LIBS := -lpthread -lboost_system -lboost_filesystem -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lwiringPi
+LIBS := -lpthread -lboost_system -lboost_filesystem -lopencv_core -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -lwiringPi -li2c
